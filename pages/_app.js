7cc767769877eaeb4b6fa00e1c6d48c83626ec56
Diego Albitres <diego.albitres@gmail.com>
2020-08-28 17:38:50 -0500

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      <DefaultSeo titleTemplate="%s | Next.js Demo" />
+    </>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter:
+          '"Inter", system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+        "inter-var":
+          '"Inter var", system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
